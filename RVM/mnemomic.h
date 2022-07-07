59c500100b2bdc730ce16befc06a8fefaa0e6e08
Rycooop <45408021+Rycooop@users.noreply.github.com>
2022-07-06 21:35:47 -0700

    Add files via upload

--- RVM/mnemomic.h
+++ RVM/mnemomic.h
@@ -12,6 +12,15 @@ enum InstructionSet
 	STACKADD = 0x35,  //add a value to the top of the stack, utilized by function calls
 	STACKGRAB = 0x36,   //grab a value from the stack, this is relative to the current stack pointer
 	STACKTAKE = 0x37,  //removes the top value of the stack, acts identically to pop
+	COPY = 0x38,   //Acts as a mov does in x64 asm. Used as follows: COPY <reg* or addr>, <val*>
+	EMPTY = 0x40, //Acts as a NO-OP
+
+	//Arithmetic
+	ADD = 0x50, //Add
+	SUB = 0x60, //Subtract
+	MUL = 0x70, //Multiply
+	DIV = 0x80, //Divide
+	ZERO = 0x90, //Zero out value pointed to
 
 	//Virtual Registers
 	VIP = 0xA0,
